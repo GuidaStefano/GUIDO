1b572a9625775f507fe4fe0713e2ccf9c4197421
FrancescoPinto02 <francesco.pintop@gmail.com>
2025-06-24 18:47:03 +0200

    CR1: stub per simulare comunicazione frontend/GUIDO

--- src/intent_web_service.py
+++ src/intent_web_service.py
@@ -1,4 +1,5 @@
 import traceback
+import uuid
 import requests
 from flask import Flask, json, jsonify, redirect, request, make_response, url_for
 
@@ -12,6 +13,8 @@ from src.service.cadocs_messages import build_message
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+numReq = 0
 
 def build_intent(intent_value: str) -> CadocsIntents:
     """
@@ -79,21 +82,145 @@ def resolve():
     -----------
     json: risultato dell'analisi del messaggio
     """
-
+    global numReq
+    print("Richiesta Ricevuta")
     try:
         data = request.get_json()
-
+        if data is None:
+            print("DEBUG: get_json() returned None")
+            print("DEBUG: request.data:", request.data)
+            print("DEBUG: request.content_type:", request.content_type)
+            return jsonify({"error": "Invalid request: JSON required"}), 400
         # se c'è il campo message
         if 'message' in data:
             intent_manager = IntentManager()
             intent, entities, _, lang = intent_manager.detect_intent(data["message"])
             data = {"intent": intent.value, "entities": entities}
+        # TODO: questo è solo uno stub per provare le richieste dal frontend
+        elif 'job_id' in data:
+            if numReq < 3:
+                numReq += 1
+                return jsonify({"job_id": data['job_id'], "status": "PENDING"})
+            else:
+                return jsonify({
+                    "job_id": data['job_id'],
+                    "status": "SUCCESS",
+                    "results": {
+                        "author": "daniele_rossi",
+                        "repository": "toad-wrapper",
+                        "patterns": [
+                            {
+                                "name": "Informal Community (IC)",
+                                "description": "Usually sets of people part of an organization, with a common interest, often closely dependent on their practice. Informal interactions, usually across unbound distances.",
+                                "detected": True
+                            },
+                            {
+                                "name": "Community of Practice (CoP)",
+                                "description": "Groups of people sharing a concern, a set of problems, or a passion about a topic, who deepen their knowledge and expertise in this area by interacting frequently in the same geolocation.",
+                                "detected": False
+                            },
+                            {
+                                "name": "Formal Network (FN)",
+                                "description": "Members are rigorously selected and prescribed by management (often in the form of FG), directed according to corporate strategy and mission.",
+                                "detected": True
+                            },
+                            {
+                                "name": "Social Network (SN)",
+                                "description": "SNs can be seen as a supertype for all OSSs. To identify an SN, it is sufficient to split the structure of organizational patterns into macrostructure and microstructure.",
+                                "detected": True
+                            },
+                            {
+                                "name": "Informal Network (IN)",
+                                "description": "Looser networks of ties between individuals that happen to come in contact in the same context. Their driving force is the strength of the ties between members.",
+                                "detected": False
+                            },
+                            {
+                                "name": "Network of Practice (NoP)",
+                                "description": "A networked system of communication and collaboration connecting CoPs. Anyone can join. They span geographical and time distances alike.",
+                                "detected": True
+                            },
+                            {
+                                "name": "Formal Group (FG)",
+                                "description": "People grouped by corporations to act on (or by means of) them. Each group has an organizational goal, called mission. Compared to FN, no reliance on networking technologies, local in nature.",
+                                "detected": False
+                            },
+                            {
+                                "name": "Project Team (PT)",
+                                "description": "People with complementary skills who work together to achieve a common purpose for which they are accountable. Enforced by their organization and follow specific strategies or organizational guidelines.",
+                                "detected": True
+                            }
+                        ],
+                        "metrics": {
+                            "dispersion": {
+                                "geo_distance_variance": 11354570.939798128,
+                                "avg_geo_distance": 3433.8892193812785,
+                                "cultural_distance_variance": 22.621716233057025
+                            },
+                            "engagement": {
+                                "m_comment_per_pr": 0.0,
+                                "mm_comment_dist": 1.0,
+                                "m_watchers": 0,
+                                "m_stargazers": 0.0,
+                                "m_active": 1.0,
+                                "mm_commit_dist": 0.0,
+                                "mm_filecollab_dist": 2.0
+                            },
+                            "formality": {
+                                "m_membership_type": 1.9166666666666667,
+                                "milestones": 36,
+                                "lifetime": 6274
+                            },
+                            "longevity": 77.8,
+                            "structure": {
+                                "repo_connections": True,
+                                "follow_connections": True,
+                                "pr_connections": True
+                            }
+                        },
+                        "graph": {
+                            "nodes": [
+                                "david-durrleman",
+                                "anders9ustafsson",
+                                "cesarsouza",
+                                "alice-martin",
+                                "bob-smith",
+                                "carla-ruiz",
+                                "deepak-sharma",
+                                "elena-fischer",
+                                "fabio-garcia",
+                                "gina-lee",
+                                "haruto-yamada",
+                                "ivan-petrov"
+                            ],
+                            "edges": [
+                                {"source": "david-durrleman", "target": "anders9ustafsson", "weight": 1},
+                                {"source": "david-durrleman", "target": "alice-martin", "weight": 2},
+                                {"source": "anders9ustafsson", "target": "cesarsouza", "weight": 3},
+                                {"source": "alice-martin", "target": "bob-smith", "weight": 1},
+                                {"source": "carla-ruiz", "target": "david-durrleman", "weight": 2},
+                                {"source": "deepak-sharma", "target": "carla-ruiz", "weight": 2},
+                                {"source": "elena-fischer", "target": "alice-martin", "weight": 1},
+                                {"source": "fabio-garcia", "target": "gina-lee", "weight": 1},
+                                {"source": "gina-lee", "target": "haruto-yamada", "weight": 3},
+                                {"source": "haruto-yamada", "target": "ivan-petrov", "weight": 2},
+                                {"source": "bob-smith", "target": "fabio-garcia", "weight": 1},
+                                {"source": "deepak-sharma", "target": "elena-fischer", "weight": 1},
+                                {"source": "ivan-petrov", "target": "carla-ruiz", "weight": 2}
+                            ]
+                        }
+                    }
+                })
+        # TODO: questo è solo uno stub per provare le richieste dal frontend
+        elif 'author' in data and 'repository' in data and 'end_date' in data:
+            job_id = str(uuid.uuid4())
+            numReq = 0
+            return jsonify({"job_id": job_id}), 200
         else:
             # se non c'è message è probabilmente un geo-dispersion
             data = {"intent": "geodispersion", "entities": data['entities']}
             lang = " "
 
-    except:
+    except :
         return jsonify({"error": "Invalid request: JSON required"}), 400
 
     print("STO PROCESSANDO RISPOSTA...")
